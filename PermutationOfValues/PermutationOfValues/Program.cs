5325cb0c6c372955c58b0fd368d39d3459456b68
HOMEPC\anton <antonkuzan@gmail.com>
2025-04-10 21:46:08 +0300

    Added 3 homeworks (Images, Permutation, Strings)

---
+++ PermutationOfValues/PermutationOfValues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            
+            string firstName = "Пупкин";
+            string lastName = "Василий";
+
+            Console.WriteLine($"Имя: {firstName}, Фамилия: {lastName}");
+            Console.WriteLine("Нажмите любую клавишу, чтобы поменять Имя и Фамилию местами...");
+            Console.ReadKey();
+
+            (firstName, lastName) = (lastName, firstName);
+                
+            Console.WriteLine($"Имя и Фамилия поменялись местами.\nТеперь Имя: {firstName}, Фамилия: {lastName}");
+        }
+    }
+}
