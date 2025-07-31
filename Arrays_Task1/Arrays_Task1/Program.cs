7eced38464067eca75377733fdf68a3d59213d96
HOMEPC\anton <antonkuzan@gmail.com>
2025-07-31 16:37:37 +0300

    Added Array_Task1

---
+++ Arrays_Task1/Arrays_Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = { { 2, 3, 7 }, { 12, 3, 5 } };
+        int sum = 0;
+        int multiply = 1;
+
+        Console.WriteLine("Existing array: ");
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+                if (i == 1)
+                {
+                   sum += array[i, j];
+                }
+            }
+            multiply *= array[i, 0];
+            Console.WriteLine();
+        }
+
+        Console.Write("\nSum of the second line: ");
+        Console.WriteLine(sum);
+        
+        Console.Write("Product of the first column: ");
+        Console.WriteLine(multiply);
+        
+       
+
+
+
+
+    }
+}
