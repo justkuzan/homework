6b42fa8dfee9d63dae10a6d876c457a4958c4033
HOMEPC\anton <antonkuzan@gmail.com>
2025-08-01 17:44:47 +0300

    Added Task3. Tasks 1 and 2 are replaced

--- Arrays_Task2/Program.cs
+++ Arrays_Task2/Program.cs
@@ -1,20 +1,21 @@
 ﻿//Наибольший элемент
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        int[,] array = new int[10,10];
+        int[,] array = new int[10, 10];
         Random random = new Random();
         int comparisonNumber = Int32.MinValue;
 
         for (int i = 0; i < array.GetLength(0); i++)
         {
-            array[i,0] = random.Next(1,6);
+            array[i, 0] = random.Next(1, 6);
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                array[i,j] = random.Next(1,6);
+                array[i, j] = random.Next(1, 6);
             }
         }
 
@@ -29,17 +30,19 @@ class Program
                 comparisonNumber = i;
             }
         }
+
         Console.Write($"\n\nLargest number in array: {comparisonNumber} ");
-        
+
         Console.WriteLine("\n\nA new matrix has been created:");
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                if (array[i,j] == comparisonNumber)
+                if (array[i, j] == comparisonNumber)
                 {
                     array[i, j] = 0;
                 }
+
                 Console.Write($"{array[i, j]} ");
             }
         }
---
+++ Arrays_Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Локальные максимумы
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[10];
+        Random rnd = new Random();
+
+        Console.WriteLine("Generated array: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(0, 100);
+            Console.Write($"{array[i]} ");
+        }
+
+        Console.WriteLine("\n\nLocal maxima: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > array[i + 1] && array[i] > array[i + 1])
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
