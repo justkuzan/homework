ffbc7bf9381a84282a8b8427e3b537b8f69af954
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-08-28 14:08:16 +0300

    Task5 added

---
+++ Arrays_Task5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Подмассив повторений чисел
+
+namespace Arrays_Task5;
+
+public class Program
+{
+    public static void Main()
+    {
+        var array = new int[30];
+        var random = new Random();
+
+        var numberOfRepetitions = 1;
+        var maxRepetitions = 1;
+        var repeatingNumber = array[0];
+
+        Console.WriteLine("Array: ");
+        for (var i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1,5);
+            Console.Write(array[i] + " ");
+        }
+
+        for (var i = 1; i < array.Length; i++)
+            if (array[i] == array[i - 1])
+            {
+                numberOfRepetitions++;
+                if (numberOfRepetitions > maxRepetitions)
+                {
+                    maxRepetitions = numberOfRepetitions;
+                    repeatingNumber = array[i];
+                }
+            }
+            else
+            {
+                numberOfRepetitions = 1;
+            }
+
+        Console.WriteLine();
+        Console.WriteLine($"Число {repeatingNumber} повторяется {maxRepetitions} раз подряд");
+    }
+}
