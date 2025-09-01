91f74a65a50f01e9d9276b8d6850322ac963f04c
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-01 13:48:23 +0300

    Array_Task6 added

---
+++ Array_Task6/Program.cs
@@ -0,0 +1,35 @@
+﻿//Сортировка чисел
+
+class Program
+{
+    public static void Main()
+    {
+        var array = new int[20];
+        var random = new Random();
+        int temp;
+
+        Console.WriteLine("Array before sorting: ");
+        for (var k = 0; k < array.Length; k++)
+        {
+            array[k] = random.Next(10);
+            Console.Write(array[k] + " ");
+        }
+        
+        for (var i = 1; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length-i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    temp = array[j+1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        
+        Console.WriteLine("\nArray after sorting: ");
+        foreach (var t in array)
+            Console.Write(t + " ");
+    }
+}
