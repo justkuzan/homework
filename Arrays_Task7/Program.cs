9f983e6992732bf89a24e868f5f67e3f2402dca3
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-04 13:19:49 +0300

    Added Arrays_Task7

---
+++ Arrays_Task7/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string guardPhrase = GetPhraseFromFile(); 
+        char[] separators = new char[] { ' ', '.',',', '!','-'};
+        string[] guardPhraseArray = guardPhrase.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        
+        foreach (var phrase in guardPhraseArray)
+        {
+            Console.WriteLine(phrase);
+        }
+    }
+
+    public static string GetPhraseFromFile()
+    {
+        string[] lines = File.ReadAllLines("TESOblivionGuardsPhrases.txt");
+        Random rnd = new Random();
+        string phrase = lines[rnd.Next(lines.Length)];
+        Console.WriteLine(phrase);
+        return phrase;
+    }
+}
