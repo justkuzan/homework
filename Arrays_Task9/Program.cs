80286c80624a17c3fb2847f4e32e59abb8280de9
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-09-12 14:30:35 +0300

    Addwd Arrays_Task9

---
+++ Arrays_Task9/Program.cs
@@ -0,0 +1,52 @@
+﻿//Скобочное выражение
+
+class Program
+{
+    public static void Main()
+    {
+        int depth;
+        
+        Random rnd = new Random();
+        char[] arr = new char[rnd.Next(4, 12)];
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (rnd.Next(2) == 0)
+                arr[i] = '(';
+            else
+                arr[i] = ')';
+
+            Console.Write(arr[i] + " ");
+        }
+
+        if (isCorrect(arr, out depth))
+            Console.WriteLine($"\nCorrect. Max depth {depth}");
+        else
+            Console.WriteLine("\nIncorrect");
+    }
+
+    static bool isCorrect(char[] arr, out int depth)
+    {
+        int alignment = 0;
+        depth = 0;
+
+        foreach (var i in arr)
+        {
+            if (i == '(')
+            {
+                alignment++;
+                if (alignment > depth)
+                    depth = alignment;
+            }
+            else if (i == ')')
+                alignment--;
+            
+            if (alignment < 0)
+                return false;
+        }
+        return alignment == 0;
+    }
+
+
+    
+}
